d5730ab39d44203543ec4cae2cb32563d377d0f3
Roman <pryiadka.roman@gmail.com>
2023-02-23 10:32:48 +0300

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close'),
+      menuItem = document.querySelectorAll('.menu__list li')
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active')
@@ -9,3 +10,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active')
 });
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active')
+        })
+});
+
